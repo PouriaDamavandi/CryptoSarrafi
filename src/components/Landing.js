ee19cd0eeeced4b70c6d3c6216ec95dfc12fe1ec
PouriaDamavandi <damavandipouria@gmail.com>
2022-08-05 23:21:05 +0430

    add Loader

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,21 +1,36 @@
 import { useEffect, useState } from "react";
 
-//API 
+//API
 import { getCoins } from "../services/api";
 
+// Components
+import Loader from "./Loader";
 
 const Landing = () => {
   useEffect(() => {
     const fetchAPI = async () => {
       const data = await getCoins();
-      console.log(data);
-    }
-      fetchAPI();
-    ;
-  },[]);
+      setCoins(data);
+    };
+    fetchAPI();
+  }, []);
 
-  return <div>Landing Page</div>;
+  const [coins, setCoins] = useState([]);
+
+  return (
+    <>
+      <input type="text" placeholder="Search" />
+      {coins.length ? (
+        <div>
+          {coins.map((coin) => (
+            <p key={coin.id}>{coin.name}</p>
+          ))}
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </>
+  );
 };
-
 
 export default Landing;
---
+++ src/components/Loader.js
@@ -0,0 +1,17 @@
+
+//Gif
+
+import Loading from "../assets/gif/Loading.gif";
+
+const Loader = () => {
+
+
+    return(
+        <div>
+            <img src={Loading} alt="loading" />
+            <h1>Loading ...</h1>
+        </div>
+    )
+}
+
+export default Loader;
